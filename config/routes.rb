57a291b1ed98e46c98c1c67ad333f497886512fa
Jacobknew <Jacobknew@gmail.com>
2013-03-14 04:56:29 -0700

    fixed default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 StrategyStroller::Application.routes.draw do
   devise_for :users, :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 
-
-  # For devise. "Ensure you have defined root_url to *something* in your config/routes.rb"
-  root :to => "home#index"
-
   match "controller_unit/controller_panel" => 'controller_unit#controller_panel', :as => 'controller_panel'
   match "provider/provider_panel" => 'provider#provider_panel', :as => 'provider_panel'
   match "admin/admin_panel" => 'admin#admin_panel', :as => 'admin_panel'
@@ -65,8 +61,10 @@ StrategyStroller::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'authentication#login'
   # See how all your routes lay out with "rake routes"
+
+  # For devise. "Ensure you have defined root_url to *something* in your config/routes.rb"
+  root :to => "devise/sessions#new" #login page
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
