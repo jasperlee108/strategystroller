47257793be1e1d497204a1314a778ec6055c9afc
Jacobknew <Jacobknew@gmail.com>
2013-03-14 01:27:03 -0700

    enabled my initial set of devise modules and their corresponding migration actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :rememberable, and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :trackable, :validatable, #end defaults (I removed rememberable, so no cookies. can add in later.)
+         :confirmable, :timeoutable, :lockable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
