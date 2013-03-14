85f63fee5be9d341ef00e7778eca39f17902618a
Jacobknew <Jacobknew@gmail.com>
2013-03-14 00:54:02 -0700

    ran devise user creation generator, nothing changed from default so far

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 StrategyStroller::Application.routes.draw do
+  devise_for :users
+
   # For devise. "Ensure you have defined root_url to *something* in your config/routes.rb"
   root :to => "home#index"
 
