f319099dcb6d1343582d59ac8c0fa20d2a0c55d4
Jacobknew <Jacobknew@gmail.com>
2013-03-13 20:02:16 -0700

    modified user model and migration partially

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,19 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :lockable,  and :omniauthable
+  devise :database_authenticatable, :registerable, :token_authenticatable, :timeoutable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   #do exception error handling
 
   belongs_to :company
-  attr_accessible :company_id, :name, :password, :position
+  attr_accessible :name, :position
   # position 0 => admin, 1 => CU, 2 => provider
   validates :name, :presence => true,
                     :length => { :maximum => 128 }
-  validates_uniqueness_of :name, :scope => :company_id
+  validates_uniqueness_of :name
   # a company has unique users
   # validates :password, :presence => true,
   #	 				:length => { :maximum => 128 }
@@ -26,15 +25,6 @@ class User < ActiveRecord::Base
     ERR_USER_EXISTS = -2
     ERR_BAD_USERNAME = -3
     ERR_BAD_PASSWORD = -4
-
-    def self.validate_login(name, password, company_id)
-        user = User.find_by_name_and_company_id(name, company_id)
-        if (!user || user.password != password)
-            return ERR_BAD_CREDENTIALS
-        else
-            return SUCCESS
-        end
-    end
 
     def self.get_position(name, password, company_id)
       user = User.find_by_name_and_company_id(name, company_id)
