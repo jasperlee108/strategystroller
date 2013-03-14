3055de90785175498557a69aaef0b4a0de3b5d1c
fdjunaidi <fdjunaidi@berkeley.edu>
2013-03-14 05:38:08 -0700

    Added the default test for authentication_controller.

--- app/controllers/tester_controller.rb
+++ app/controllers/tester_controller.rb
@@ -6,6 +6,8 @@ class TesterController < ApplicationController
   def runRspecTest
     
     ## Add further system calls as you add more Rspec tests
+    
+    ## UNIT TEST -- Model
     ## Template: system("rspec ./spec/models/xxx_spec.rb")
     
     system("echo Running goal_spec test..")
@@ -19,6 +21,12 @@ class TesterController < ApplicationController
     
     system("echo Running admin_user_spec test..")
     system("rspec ./spec/models/admin_user_spec.rb")
+    
+    ## FUNCTIONAL TEST -- Controller
+    ## Template: system("rspec ./spec/controllers/xxx_spec.rb")
+    
+    system("echo Running authentication_controller_spec test..")
+    system("rspec ./spec/controllers/authentication_controller_spec.rb")
     
   end
 end
