da96a91601756eb6ebccec0207e13dc8d0048c0d
Jacobknew <Jacobknew@gmail.com>
2013-03-14 03:20:29 -0700

    added controlling_unit boolean field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130314093409) do
+ActiveRecord::Schema.define(:version => 20130314101405) do
 
   create_table "activities", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -51,8 +51,8 @@ ActiveRecord::Schema.define(:version => 20130314093409) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.integer  "sign_in_count",          :default => 0
@@ -67,9 +67,10 @@ ActiveRecord::Schema.define(:version => 20130314093409) do
     t.integer  "failed_attempts",        :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "username"
+    t.boolean  "controlling_unit",       :default => false
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
