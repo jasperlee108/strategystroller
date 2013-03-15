69503de083a9cc196e572db49afd0eeacaa05787
fdjunaidi <fdjunaidi@berkeley.edu>
2013-03-15 05:57:43 -0700

    Add functional tests for authentication controller. All working ok. Updated testing framework.

--- app/controllers/tester_controller.rb
+++ app/controllers/tester_controller.rb
@@ -3,30 +3,43 @@ class TesterController < ApplicationController
   ## Can use this from command line using the following:
   ## "rails console"
   ## "TesterController.new.runRspecTest"
-  def runRspecTest
+  def runRspecTest(test)
     
     ## Add further system calls as you add more Rspec tests
+    if test.empty?
+      test = 'all'
+    end
     
     ## UNIT TEST -- Model
     ## Template: system("rspec ./spec/models/xxx_spec.rb")
     
-    system("echo Running goal_spec test..")
-    system("rspec ./spec/models/goal_spec.rb")
-
-    system("echo Running user_spec test..")
-    system("rspec ./spec/models/user_spec.rb")
+    if test == 'goal' or test == 'all'
+      system("echo Running goal_spec test..")
+      system("rspec ./spec/models/goal_spec.rb")
+    end
     
-    system("echo Running company_spec test..")
-    system("rspec ./spec/models/company_spec.rb")
+    if test == 'user' or test == 'all'
+      system("echo Running user_spec test..")
+      system("rspec ./spec/models/user_spec.rb")
+    end
     
-    system("echo Running admin_user_spec test..")
-    system("rspec ./spec/models/admin_user_spec.rb")
+    if test == 'company' or test == 'all'
+      system("echo Running company_spec test..")
+      system("rspec ./spec/models/company_spec.rb")
+    end
+    
+    if test == 'admin' or test == 'all'
+      system("echo Running admin_user_spec test..")
+      system("rspec ./spec/models/admin_user_spec.rb")
+    end
     
     ## FUNCTIONAL TEST -- Controller
     ## Template: system("rspec ./spec/controllers/xxx_spec.rb")
     
-    system("echo Running authentication_controller_spec test..")
-    system("rspec ./spec/controllers/authentication_controller_spec.rb")
+    if test == 'authentication' or test == 'all'
+      system("echo Running authentication_controller_spec test..")
+      system("rspec ./spec/controllers/authentication_controller_spec.rb")
+    end
     
   end
 end
--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -2,12 +2,23 @@ require 'spec_helper'
 
 describe AuthenticationController do
 
+  ## DEFAULT: Login & Logout
+
   describe "GET 'login'" do
     it "returns http success" do
       get 'login'
       response.should be_success
     end
   end
+
+  describe "GET 'logout'" do
+    it "returns http success" do
+      get 'logout'
+      response.should be_success
+    end
+  end
+
+  ## PENDING: Not yet implemented, so should pass as is
 
   describe "GET 'create_user'" do
     it "returns http success" do
@@ -23,11 +34,127 @@ describe AuthenticationController do
     end
   end
 
-  describe "GET 'logout'" do
-    it "returns http success" do
-      get 'logout'
-      response.should be_success
+  ### NOTE: Using a mix of ruby format and Rspec format
+  ### PURPOSE: To test if redirection works as expected
+
+  CU = 0
+  PROVIDER = 1
+  STRATEGYSTROLLER = 1
+  PETERDUERR = 2
+
+  ## LOGIN - CU
+  
+  # CU Exists
+  describe "POST 'login'" do
+    it "should let existing CU logs in" do
+      user = User.new(
+        :name => "james",
+        :password => "bond",
+        :company_id => STRATEGYSTROLLER,
+        :position => CU
+      ).save
+      post "login",
+        :username => "james",
+        :password => "bond",
+        :company => { :name => STRATEGYSTROLLER.to_s }
+      expect(response).to redirect_to("/controller_unit/controller_panel")
     end
+  end
+
+  # CU Does Not Exist
+  describe "POST 'login'" do
+    it "should let existing CU logs in" do
+      user = User.new(
+        :name => "james",
+        :password => "bond",
+        :company_id => STRATEGYSTROLLER,
+        :position => CU
+      ).save
+      post "login",
+        :username => "andy",
+        :password => "warhol",
+        :company => { :name => STRATEGYSTROLLER.to_s }
+      expect(response).to redirect_to("/authentication/login")
+    end
+  end
+
+  # CU Wrong Company
+  describe "POST 'login'" do
+    it "should let existing CU logs in" do
+      user = User.new(
+        :name => "james",
+        :password => "bond",
+        :company_id => STRATEGYSTROLLER,
+        :position => CU
+      ).save
+      post "login",
+        :username => "james",
+        :password => "bond",
+        :company => { :name => PETERDUERR.to_s }
+      expect(response).to redirect_to("/authentication/login")
+    end
+  end
+
+  ## LOGIN - Provider
+  
+  # Provider Exists
+  describe "POST 'login'" do
+    it "should let existing CU logs in" do
+      user = User.new(
+        :name => "andy",
+        :password => "warhol",
+        :company_id => STRATEGYSTROLLER,
+        :position => PROVIDER
+      ).save
+      post "login",
+        :username => "andy",
+        :password => "warhol",
+        :company => { :name => STRATEGYSTROLLER.to_s }
+      expect(response).to redirect_to("/provider/provider_panel")
+    end
+  end
+
+  # Provider Does Not Exist
+  describe "POST 'login'" do
+    it "should let existing CU logs in" do
+      user = User.new(
+        :name => "andy",
+        :password => "warhol",
+        :company_id => STRATEGYSTROLLER,
+        :position => PROVIDER
+      ).save
+      post "login",
+        :username => "james",
+        :password => "bond",
+        :company => { :name => STRATEGYSTROLLER.to_s }
+      expect(response).to redirect_to("/authentication/login")
+    end
+  end
+
+  # Provider Wrong Company
+  describe "POST 'login'" do
+    it "should let existing CU logs in" do
+      user = User.new(
+        :name => "andy",
+        :password => "warhol",
+        :company_id => STRATEGYSTROLLER,
+        :position => PROVIDER
+      ).save
+      post "login",
+        :username => "andy",
+        :password => "warhol",
+        :company => { :name => PETERDUERR.to_s }
+      expect(response).to redirect_to("/authentication/login")
+    end
+  end
+
+  ### EXTRA
+  
+  ## Running Rspec remotely (RoR)
+  ## Credit: Yong Hoon Lee
+  ## Source: https://sites.google.com/a/eecs.berkeley.edu/cs169-sp13/project/setting-up-a-deployment-site
+  after(:each) do
+    User.delete_all
   end
 
 end
