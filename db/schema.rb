8f9e4127be27d41025ae8dbd4143eb1d5b7ff2f3
jshizzle108 <jasperlee@berkeley.edu>
2013-04-16 00:36:45 -0700

    Added final and reviewed field to forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130416070130) do
+ActiveRecord::Schema.define(:version => 20130416073621) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -92,6 +92,8 @@ ActiveRecord::Schema.define(:version => 20130416070130) do
     t.integer  "goal_id"
     t.integer  "indicator_id"
     t.integer  "project_id"
+    t.boolean  "reviewed"
+    t.boolean  "final"
   end
 
   create_table "forms_users", :force => true do |t|
