e75bc7b4a1125e408c0479e82dd7a77b8b553de1
Jacobknew <Jacobknew@gmail.com>
2013-04-24 20:04:43 -0700

    added a basic db.seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+AdminUser.create(
+    :email => 'admin@example.com',
+    :password => 'password',
+    :password_confirmation => 'password'
+)
+
+cu = User.new(
+    :email => 'cu@example.com',
+    :password => 'password',
+    :business_code => 'A1',
+    :password_confirmation => 'password',
+    :controlling_unit => true,
+    :username => "CU"
+)
+cu.skip_confirmation!
+cu.save
+
+provider = User.new(
+    :email => 'provider@example.com',
+    :password => 'password',
+    :business_code => 'A1',
+    :password_confirmation => 'password',
+    :controlling_unit => false,
+    :username => "Provider"
+)
+provider.skip_confirmation!
+provider.save
+
