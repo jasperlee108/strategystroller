1e71b6bd0d549172847b5ba7d17aa2ef10087c9e
jshizzle108 <jasperlee@berkeley.edu>
2013-04-25 16:24:04 -0700

    fixed mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 StrategyStroller::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :domain             => 'gmail.com', #you can also use google.com
+  :authentication     => :plain,
+  :user_name          => 'strategystroller@gmail.com',
+  :password           => 'cs169squad'
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,14 @@ StrategyStroller::Application.configure do
   # In production, :host should be set to the actual host of your application."
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "strategystroller",
+    password: "cs169squad"
+  }
+
 end
