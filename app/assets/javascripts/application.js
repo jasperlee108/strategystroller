7671dc2fbc3a383567d352ad85ce272dfc51dbfb
fdjunaidi <fdjunaidi@berkeley.edu>
2013-04-30 05:18:20 -0700

    Remove twitter/bootstrap in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,6 @@
 
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap/alert
-//= require twitter/bootstrap/button
-//= require twitter/bootstrap/dropdown
-//= require twitter/bootstrap/popover
 //= require cocoon
 //= require_directory .
 
