09d9f453e4952fb3e202279877c2d6a0370de330
jshizzle108 <jasperlee@berkeley.edu>
2013-05-03 00:14:32 -0700

    graph fixes

--- app/controllers/controller_unit_controller.rb
+++ app/controllers/controller_unit_controller.rb
@@ -42,14 +42,14 @@ class ControllerUnitController < ApplicationController
       @bar_chart = Gchart.bar( 
               :axis_with_labels => 'y',
               :axis_labels => [axis_range],
-              :size => '500x500',
+              :size => '500x700',
               :theme => :pastel,
               :title => "Projects Global Status",
               :bar_width_and_spacing => spacing,
               :legend => pname_list,
               :data => clean_list(pgs_list_uc))
 
-      @pie_chart = Gchart.pie_3d(:title => 'Project Status Distribution', :size => '600x300',
+      @pie_chart = Gchart.pie_3d(:title => 'Project Status Distribution', :size => '600x500',
                 :data => pgs_list_uc, :labels => pname_list )
     end
   end
