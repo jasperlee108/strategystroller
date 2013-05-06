1ca0d701e7ed5a4cf3442a6ecfb07bf6a6298c6a
fdjunaidi <fdjunaidi@berkeley.edu>
2013-05-06 06:53:06 -0700

    Test cases for helper methods.

--- spec/helpers/controller_unit_helper_spec.rb
+++ spec/helpers/controller_unit_helper_spec.rb
@@ -1,15 +1,44 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ControllerUnitHelper. For example:
-#
-# describe ControllerUnitHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
+## INFO
+GOAL = 1
+INDICATOR = 2
+PROJECT = 3
+ERROR = 4
+ENTRY_ID = 1
+FORM_ID = 1
+
 describe ControllerUnitHelper do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  
+  ### NOTE: Using ruby format, not Rspec
+  
+  ## Default
+  it "should pass assert true sanity test" do
+    assert(true, "Did not pass sanity check")
+  end
+  
+  ## Goal path
+  it "redirect to Goal path" do
+    path = find_path_cu(GOAL, ENTRY_ID, FORM_ID)
+    assert(path == goal_check_path(:entry_id => ENTRY_ID, :form_id => FORM_ID), "Wrong path taken!")
+  end
+
+  ## Indicator path
+  it "redirect to Indicator path" do
+    path = find_path_cu(INDICATOR, ENTRY_ID, FORM_ID)
+    assert(path == indicator_check_path(:entry_id => ENTRY_ID, :form_id => FORM_ID), "Wrong path taken!")
+  end
+
+  ## Project path
+  it "redirect to Project path" do
+    path = find_path_cu(PROJECT, ENTRY_ID, FORM_ID)
+    assert(path == project_check_path(:entry_id => ENTRY_ID, :form_id => FORM_ID), "Wrong path taken!")
+  end
+  
+  ## Wrong path
+  it "redirect to Wrong path" do
+    path = find_path_cu(ERROR, ENTRY_ID, FORM_ID)
+    assert(path == cu_review_path, "Wrong path taken!")
+  end
+  
 end
